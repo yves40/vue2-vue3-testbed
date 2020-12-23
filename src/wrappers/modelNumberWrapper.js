092bd00df6a85d6817b0abf75d4bef6d848193e7
yves40 <yves.toubhans@free.fr>
2020-12-23 17:45:28 +0100

    Shoot log messages

--- src/wrappers/modelNumberWrapper.js
+++ src/wrappers/modelNumberWrapper.js
@@ -6,12 +6,10 @@ export function modelNumberWrapper(props, emit, name = 'value') {
       return parseInt(props[name]) ;
     },
     set: (value) => { 
-      console.log(`******** ${value}`);
       if(isNaN(value)) {
         emit(`${name}`, 0); 
       }
       else {
-        console.log(`Emit ${name} for ${value}`)
         emit(`${name}`, parseInt(value));
       }
     }
