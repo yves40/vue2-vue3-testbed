2913ad3c6b61a5d23fc015f292c638757d5dbc19
yves40 <yves.toubhans@free.fr>
2021-01-01 11:41:11 +0100

    Morte tests with button

--- src/views/TesterNumfield.vue
+++ src/views/TesterNumfield.vue
@@ -3,10 +3,11 @@
     <div class="moduletitle">{{Version}}</div>
     <div>
       <!-- This code is vue2 specific. Things are different in vue3. No need to write @value... -->
-      <numfield v-model="age" @value="age = $event" minvalue="10" maxvalue="120" message="Age please:"/>
-      <numfield v-model="size" @value="size = $event" maxvalue="210" message="Size please:"/>
-      <numfield v-model="weight" @value="weight = $event" minvalue="50" message="Your weight:"/>
+      <numfield v-model="age" @value="age = $event" @isvalid="agevalid = $event" minvalue="10" maxvalue="120" message="Age please:"/>
+      <numfield v-model="size" @value="size = $event" @isvalid="sizevalid = $event" maxvalue="210" message="Size please:"/>
+      <numfield v-model="weight" @value="weight = $event" @isvalid="weightvalid = $event" minvalue="50" message="Your weight:"/>
       <numfield v-model="freezone" @value="freezone = $event" message="Free number"/>
+      <button type="submit" :disabled='!buttonflag'>Ready to send</button>
       <div>
         <span>Result : </span>
         <span>{{ thesum }}</span>
@@ -20,7 +21,7 @@
 
 import numfield from "./numfield"
 
-import { onMounted, onBeforeUnmount, ref, watch, computed, onUnmounted } from "@vue/composition-api";
+import { onMounted, onBeforeUnmount, ref, watch, computed, onUnmounted,toRef } from "@vue/composition-api";
 
 export default {
   props: {
@@ -31,13 +32,18 @@ export default {
   name: 'TesterNumfield',
   setup(props, context) {
 
-    let Version = 'TesterNumfield: 1.32, Dec 23 2020 '
+    let Version = 'TesterNumfield: 1.54, Jan 01 2021 '
 
     let age = ref(5);
+    let agevalid = ref(false);
     let size = ref(175);
+    let sizevalid = ref(false);
     const weight = ref(100);
+    let weightvalid = ref(false);
     let freezone = ref(100);
-    let thesum = computed( () => age.value+size.value+weight.value+freezone.value)
+    let thesum = computed( () => age.value+size.value+weight.value+freezone.value);
+//    let buttonflag = computed( () => agevalid.value && sizevalid.value && weightvalid.value);
+    let buttonflag = computed( () => agevalid.value );
 
     // Test lifecycle handlers
     console.clear();
@@ -47,19 +53,27 @@ export default {
     //-----------------------------------------------------------------------
     // Track user actions
     //-----------------------------------------------------------------------
-    watch( [age], ([c1], [p1]) => {
-      console.debug("**** " + c1 + "/" + p1 )
+    watch( [age, agevalid], ([currentage, currentagevalid], [prevage, prevagevalid]) => {
+      console.log(Version + currentage + "/" + prevage + ' ==== ' + agevalid.value);
     })
+
+    // Check button 
+    function readyTogo() {
+      console.log('****************** ' + agevalid.value);
+      return agevalid.value;
+    }
 
     // Utilities
     function getVersion() { return  Version; }
 
     return { 
       age,
+      agevalid,
       size,
       weight,
       freezone,
       thesum,
+      buttonflag,
       Version,
     }
   }
