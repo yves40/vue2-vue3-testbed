95a060afe33a50c5eced555d5539bb6b5d1a57ba
yves40 <yves.toubhans@free.fr>
2021-03-11 18:10:25 +0100

    Debugging flx order

--- src/views/cssflextest/menu.vue
+++ src/views/cssflextest/menu.vue
@@ -9,37 +9,33 @@
             <nav>
                 <ul class="css-menu">
                     <li class="logo"><a href="#">Creative Mind Agency</a></li>
-                    <li @click="toggleMenu" ref="togglebutton" class="toggle"><a href="#"><i :class=htmlclass.innerhtml></i></a></li>
-                </ul>
-                <li class="item button"><a href="#">Log In</a></li>
-                <li class="item button secondary"><a href="#">Sign Up</a></li>
-                <div v-if="htmlclass.active">
-                    <li class="item"><a href="#">Home</a></li>
-                    <li class="item"><a href="#">About</a></li>
-                    <li class="item has-submenu">
-                        <a @click="toggleServices" tabindex="0">Services</a>
-                        <div v-if="htmlclass.activeservices">
-                            <ul class="css-submenu">
+                    <li class="toggle" @click="toggleMenu"><a href="#"><i :class=htmlclass.innerhtml></i></a></li>
+                    <div v-if="htmlclass.active">
+                        <li class="item button"><a href="#">Log In</a></li>
+                        <li class="item button"><a href="#">Sign Up</a></li>
+                        <li class="item"><a href="#">Home</a></li>
+                        <li class="item secondary"><a href="#">About</a></li>
+                        <li class="item has-submenu">
+                            <a @click="toggleServices" tabindex="0">Services</a>
+                            <ul class="css-submenu" v-if="htmlclass.activeservices">
                                 <li class="subitem"><a href="#">Service 1</a></li>
                                 <li class="subitem"><a href="#">Service 2</a></li>
                                 <li class="subitem"><a href="#">Service 3</a></li>
                                 <li class="subitem"><a href="#">Service 4</a></li>
                             </ul>
-                        </div>
-                    </li>
-                    <li class="item has-submenu">
-                        <a @click="togglePlans" tabindex="0">Plans</a>
-                        <div v-if="htmlclass.activeplans">
-                            <ul class="css-submenu">
+                        </li>
+                        <li class="item has-submenu">
+                            <a @click="togglePlans" tabindex="0">Plans</a>
+                            <ul class="css-submenu" v-if="htmlclass.activeplans">
                                 <li class="subitem"><a href="#">Plan One</a></li>
                                 <li class="subitem"><a href="#">Plan Two</a></li>
                                 <li class="subitem"><a href="#">Plan Three</a></li>
                             </ul>
-                        </div>
-                    </li>
-                    <li class="item"><a href="#">Blog</a></li>
-                    <li class="item"><a href="#">Contact</a></li>
-                 </div>
+                        </li>
+                        <li class="item"><a href="#">Blog</a></li>
+                        <li class="item"><a href="#">Contact</a></li>
+                    </div>
+                </ul>
             </nav>
         </body>
     </html>
@@ -47,11 +43,11 @@
 
 <script>
 
-import { reactive, computed } from "@vue/composition-api";
+import { reactive } from "@vue/composition-api";
 
 export default {
     setup(props) {
-        const Version = 'menu: 1.30, Mar 10 2021 '
+        const Version = 'menu: 1.34, Mar 10 2021 '
 
         let htmlclass = reactive( {
             innerhtml: 'fas fa-bars',
@@ -95,6 +91,9 @@ export default {
                htmlclass.activeplans = true; 
                htmlclass.activeservices = false;
             }
+        }
+        function onresize(UIEvent) {
+            console.log('.')
         }
 
         return {
