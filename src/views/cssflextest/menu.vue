7a50804a22a6eff25dd70cd8234e84b3cfaab262
yves40 <yves.toubhans@free.fr>
2021-03-11 18:13:31 +0100

    Done

--- src/views/cssflextest/menu.vue
+++ src/views/cssflextest/menu.vue
@@ -10,12 +10,11 @@
                 <ul class="css-menu">
                     <li class="logo"><a href="#">Creative Mind Agency</a></li>
                     <li class="toggle" @click="toggleMenu"><a href="#"><i :class=htmlclass.innerhtml></i></a></li>
-                    <div v-if="htmlclass.active">
-                        <li class="item button"><a href="#">Log In</a></li>
-                        <li class="item button"><a href="#">Sign Up</a></li>
-                        <li class="item"><a href="#">Home</a></li>
-                        <li class="item secondary"><a href="#">About</a></li>
-                        <li class="item has-submenu">
+                        <li class="item button" v-if="htmlclass.active"><a href="#">Log In</a></li>
+                        <li class="item button" v-if="htmlclass.active"><a href="#">Sign Up</a></li>
+                        <li class="item" v-if="htmlclass.active"><a href="#">Home</a></li>
+                        <li class="item secondary" v-if="htmlclass.active"><a href="#">About</a></li>
+                        <li class="item has-submenu" v-if="htmlclass.active">
                             <a @click="toggleServices" tabindex="0">Services</a>
                             <ul class="css-submenu" v-if="htmlclass.activeservices">
                                 <li class="subitem"><a href="#">Service 1</a></li>
@@ -24,7 +23,7 @@
                                 <li class="subitem"><a href="#">Service 4</a></li>
                             </ul>
                         </li>
-                        <li class="item has-submenu">
+                        <li class="item has-submenu" v-if="htmlclass.active">
                             <a @click="togglePlans" tabindex="0">Plans</a>
                             <ul class="css-submenu" v-if="htmlclass.activeplans">
                                 <li class="subitem"><a href="#">Plan One</a></li>
@@ -32,9 +31,8 @@
                                 <li class="subitem"><a href="#">Plan Three</a></li>
                             </ul>
                         </li>
-                        <li class="item"><a href="#">Blog</a></li>
-                        <li class="item"><a href="#">Contact</a></li>
-                    </div>
+                        <li class="item" v-if="htmlclass.active"><a href="#">Blog</a></li>
+                        <li class="item" v-if="htmlclass.active"><a href="#">Contact</a></li>
                 </ul>
             </nav>
         </body>
@@ -47,7 +45,7 @@ import { reactive } from "@vue/composition-api";
 
 export default {
     setup(props) {
-        const Version = 'menu: 1.34, Mar 10 2021 '
+        const Version = 'menu: 1.36, Mar 10 2021 '
 
         let htmlclass = reactive( {
             innerhtml: 'fas fa-bars',
