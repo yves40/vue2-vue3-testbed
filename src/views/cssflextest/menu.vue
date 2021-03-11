a58196c60fe2d895f3c3c12af3d5b399738dda91
yves40 <yves.toubhans@free.fr>
2021-03-11 12:19:26 +0100

    Works in a Vue3 !

--- src/views/cssflextest/menu.vue
+++ src/views/cssflextest/menu.vue
@@ -9,32 +9,38 @@
             <nav>
                 <ul class="css-menu">
                     <li class="logo"><a href="#">Creative Mind Agency</a></li>
+                    <li @click="toggleMenu" ref="togglebutton" class="toggle"><a href="#"><i :class=htmlclass.innerhtml></i></a></li>
+                </ul>
+                <div v-if="htmlclass.active">
                     <li class="item"><a href="#">Home</a></li>
                     <li class="item"><a href="#">About</a></li>
                     <li class="item has-submenu">
-                        <a tabindex="0">Services</a>
-                        <ul class="css-submenu">
-                            <li class="subitem"><a href="#">Design</a></li>
-                            <li class="subitem"><a href="#">Development</a></li>
-                            <li class="subitem"><a href="#">SEO</a></li>
-                            <li class="subitem"><a href="#">Copywriting</a></li>
-                        </ul>
+                        <a @click="toggleServices" tabindex="0">Services</a>
+                        <div v-if="htmlclass.activeservices">
+                            <ul class="css-submenu">
+                                <li class="subitem"><a href="#">Service 1</a></li>
+                                <li class="subitem"><a href="#">Service 2</a></li>
+                                <li class="subitem"><a href="#">Service 3</a></li>
+                                <li class="subitem"><a href="#">Service 4</a></li>
+                            </ul>
+                        </div>
                     </li>
                     <li class="item has-submenu">
-                        <a tabindex="0">Plans</a>
-                        <ul class="css-submenu">
-                            <li class="subitem"><a href="#">Freelancer</a></li>
-                            <li class="subitem"><a href="#">Startup</a></li>
-                            <li class="subitem"><a href="#">Enterprise</a></li>
-                        </ul>
+                        <a @click="togglePlans" tabindex="0">Plans</a>
+                        <div v-if="htmlclass.activeplans">
+                            <ul class="css-submenu">
+                                <li class="subitem"><a href="#">Plan One</a></li>
+                                <li class="subitem"><a href="#">Plan Two</a></li>
+                                <li class="subitem"><a href="#">Plan Three</a></li>
+                            </ul>
+                        </div>
                     </li>
                     <li class="item"><a href="#">Blog</a></li>
                     <li class="item"><a href="#">Contact</a>
                     </li>
                     <li class="item button"><a href="#">Log In</a></li>
                     <li class="item button secondary"><a href="#">Sign Up</a></li>
-                    <li @click="toggleMenu" ref="togglebutton" class="toggle"><a href="#"><i :class=htmlclass.innerhtml></i></a></li>
-                </ul>
+                 </div>
             </nav>
         </body>
     </html>
@@ -46,35 +52,57 @@ import { reactive, computed } from "@vue/composition-api";
 
 export default {
     setup(props) {
-        const Version = 'menu: 1.21, Mar 10 2021 '
+        const Version = 'menu: 1.30, Mar 10 2021 '
 
         let htmlclass = reactive( {
-            innerhtml: 'fas fa-bars'
+            innerhtml: 'fas fa-bars',
+            active: false,
+            activeservices: false,
+            activeplans: false
         });
         const inactivebar = "fas fa-bars";
         const activebar = "fas fa-times";
-        let active = false;
 
 
         console.clear();
         console.log(Version);
 
         function toggleMenu() {
-            if (!active) {
-                active = true;
+            if (!htmlclass.active) {
+                htmlclass.active = true;
                 htmlclass.innerhtml = activebar;
+                htmlclass.activeservices = false;
             }
             else {
-                active = false;
-                console.log('Set innerhtml to ' + inactivebar);
+                htmlclass.active = false;
                 htmlclass.innerhtml = inactivebar;
+                htmlclass.activeservices = false;
             }
         }
-        // toggle.addEventListener('click', toggleMenu, false);
+        function toggleServices() {
+            if(htmlclass.activeservices) {
+               htmlclass.activeservices = false; 
+            }
+            else {
+               htmlclass.activeservices = true; 
+               htmlclass.activeplans = false;
+            }
+        }
+        function togglePlans() {
+            if(htmlclass.activeplans) {
+               htmlclass.activeplans = false; 
+            }
+            else {
+               htmlclass.activeplans = true; 
+               htmlclass.activeservices = false;
+            }
+        }
 
         return {
             Version, 
             toggleMenu,
+            toggleServices,
+            togglePlans,
             htmlclass
         }
     }
